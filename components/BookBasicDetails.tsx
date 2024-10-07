42e2496f14f2f56fc71db862428aff2373bbc136
Martin <martinsutch@gmail.com>
2024-10-07 15:31:04 +0100

    Basic book adding completed

--- components/BookBasicDetails.tsx
+++ components/BookBasicDetails.tsx
@@ -1,53 +1,51 @@
 import React from "react";
-import { Text, View, Image , StyleSheet, Dimensions } from "react-native";
+import { Text, View, Image, StyleSheet, Dimensions } from "react-native";
 
 const { width } = Dimensions.get("window");
-const cardWidth = width / 3 - 10
-const cardHeight = cardWidth * 1.5
+const cardWidth = width / 3 - 10;
+const cardHeight = cardWidth * 1.5;
 
-const BookBasicDetails = ({book}) => {
+const BookBasicDetails = ({ book }) => {
+  const authorsString = book.authors.join(", ");
 
-    const authorsString = book.authors.join(", ")
-
-    return (
+  return (
     <View style={styles.outerFrame}>
-        <Image style={styles.image} source={{ uri: book.thumbnail }} />
-        <View style={styles.innerFrame}>
-            <Text style={styles.title}>{book.title}</Text>
-            <Text style={styles.authors}>{authorsString}</Text>
-        </View>
+      <Image style={styles.image} source={{ uri: book.thumbnail }} />
+      <View style={styles.innerFrame}>
+        <Text style={styles.title}>{book.title}</Text>
+        <Text style={styles.authors}>{authorsString}</Text>
+      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-    outerFrame: {
-        flex: 1,
-        flexDirection: "row",
-    },
-    image: {
-        margin: 5,
-        borderRadius: 5,
-        borderWidth: 1,
-        borderColor: "black",
-        width: cardWidth,
-        height: cardHeight,
-        resizeMode: "cover",
-    },
-    innerFrame: {
-        flex: 1,
-        flexDirection: "column",
-        justifyContent: "flex-end"
-    },
-    title: {
-        fontSize: 32,
-        fontWeight: "bold",
-    },
-    authors: {
-        fontSize: 16,
-        fontWeight: "bold",
-    }
-  
-  });
+  outerFrame: {
+    flex: 1,
+    flexDirection: "row",
+  },
+  image: {
+    margin: 5,
+    borderRadius: 5,
+    borderWidth: 1,
+    borderColor: "black",
+    width: cardWidth,
+    height: cardHeight,
+    resizeMode: "cover",
+  },
+  innerFrame: {
+    flex: 1,
+    flexDirection: "column",
+    justifyContent: "flex-end",
+  },
+  title: {
+    fontSize: 32,
+    fontWeight: "bold",
+  },
+  authors: {
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+});
 
 export default BookBasicDetails;
