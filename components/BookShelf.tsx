d1a6d68dfcb9eb22db0d725be2f121aaa95facc5
Martin <martinsutch@gmail.com>
2024-10-08 15:20:19 +0100

    Home Screen Inital Creation

---
+++ components/BookShelf.tsx
@@ -0,0 +1,69 @@
+import React from "react";
+import { ScrollView, Text, StyleSheet, View } from "react-native";
+import BookCard from "./BookCard";
+
+type BookShelfProps = {
+  books: Book[];
+  page: string;
+};
+
+const BookShelf = ({ books, page }: BookShelfProps) => {
+  return (
+    <View style={styles.outerContainer}>
+      <View style={styles.container}>
+        {books.length === 0 ? (
+          <Text style={styles.comment}>
+            {page === "borrowing"
+              ? "You're not currently borrowing any books."
+              : "You're not currently lending any books."}
+          </Text>
+        ) : (
+          <ScrollView
+            horizontal
+            showsHorizontalScrollIndicator={false}
+            contentContainerStyle={styles.scrollContainer}
+          >
+            {books.map((book) => (
+              <View style={styles.bookContainer} key={book.bookId}>
+                <BookCard isLoaded={true} book={book} page={page} />
+                <Text style={styles.comment}>Comment</Text>
+              </View>
+            ))}
+          </ScrollView>
+        )}
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  outerContainer: {
+    padding: 10,
+  },
+  container: {
+    padding: 10,
+    backgroundColor: "white",
+    borderRadius: 10,
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2,
+    shadowRadius: 5,
+    elevation: 3,
+  },
+  scrollContainer: {
+    paddingVertical: 10,
+    flexDirection: "row",
+  },
+  bookContainer: {
+    marginRight: 10,
+    alignItems: "center",
+  },
+  comment: {
+    marginVertical: 5,
+    fontSize: 12,
+    color: "grey",
+    textAlign: "center",
+  },
+});
+
+export default BookShelf;
--- components/HomeScreen.tsx
+++ components/HomeScreen.tsx
@@ -1,12 +1,66 @@
-import React from "react";
-import { Text, View } from "react-native";
+import React, { useContext, useEffect, useState } from "react";
+import { Text, View, StyleSheet } from "react-native";
+import StatBlock from "./StatBlock";
+import { UserContext } from "../contexts/UserContext";
+import BookShelf from "./BookShelf";
+import functions from "../axiosRequests";
 
 const HomeScreen = () => {
+  const { user } = useContext(UserContext);
+  const [booksBorrorwing, setBooksBorrowing] = useState([]);
+  const [booksLending, setBooksLending] = useState([]);
+  const currentHour = new Date().getHours();
+  let greeting = "";
+
+  if (currentHour < 12) {
+    greeting = `Good morning, ${user.username}!`;
+  } else if (currentHour < 18) {
+    greeting = `Good afternoon, ${user.username}!`;
+  } else {
+    greeting = `Good evening, ${user.username}!`;
+  }
+
+  useEffect(() => {
+    functions.getBorrowingList(user.username).then((response) => {
+      console.log(response);
+      setBooksBorrowing(response);
+    });
+  }, []);
+
+  useEffect(() => {
+    functions.getLendingList(user.username).then((response) => {
+      setBooksLending(response);
+    });
+  }, []);
+
   return (
-    <View>
-      <Text>Home Page</Text>
+    <View style={styles.page}>
+      <View style={styles.container}>
+        <Text style={styles.greeting}>{greeting}</Text>
+      </View>
+      <StatBlock friend={user} />
+      <Text>Books you're borrowing:</Text>
+      <BookShelf books={booksBorrorwing} page={"borrowing"} />
+      <Text>Books you're lending:</Text>
+      <BookShelf books={booksLending} page={"lending"} />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  page: {
+    padding: 10,
+  },
+  container: {
+    alignItems: "center",
+    justifyContent: "center",
+    marginVertical: 20,
+  },
+  greeting: {
+    fontSize: 20,
+    fontWeight: "bold",
+    color: "darkslategray",
+  },
+});
 
 export default HomeScreen;
